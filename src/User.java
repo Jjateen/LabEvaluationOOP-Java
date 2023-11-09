80a828b1cc2ce9d56b1d44659f0a09e53b2e6d3d
Jjateen Gundesha <116161686+Jjateen@users.noreply.github.com>
2023-11-09 16:26:30 +0530

    Update User.java

--- src/User.java
+++ src/User.java
@@ -6,6 +6,10 @@ public class User {
     String password;
     String userRole;
     int userID;
+
+    //Static ArrayList to store and maintain count of employees
+    static ArrayList<Employee> a = new ArrayList<>();
+
     User(int userID,String userName,String password,String userRole){
         this.userID=userID;
         this.userName=userName;
@@ -42,16 +46,5 @@ public class User {
         this.userName=userName;
         this.password=password;
         this.userRole=userRole;
-    }
-
-    //Static ArrayList to store and maintain count of employees
-    static ArrayList<Employee> a = new ArrayList<>();
-    Employee getEmployeeByID(int userID) {
-        for (Employee e: a) {
-            if (e.userID == userID) {
-                return e;
-            }
-        }
-        return null;
     }
 }
