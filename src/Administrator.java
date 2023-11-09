792c5f3334f084e90cc3c6487291a607995d37d4
Jjateen Gundesha <116161686+Jjateen@users.noreply.github.com>
2023-11-09 16:24:11 +0530

    Update Administrator.java

--- src/Administrator.java
+++ src/Administrator.java
@@ -16,9 +16,30 @@ public class Administrator extends User {
         System.out.println("Enter qualification"); String q = sc.nextLine();
         Employee e = new Employee(ui,un,pass,ur,d,cc,n,q);
     }
-    void defineAccessLevel(){
-        System.out.println("Access Level Defined");
+    void defineAccessLevel(Employee e, String level){
+        switch(level) {
+            case "private":
+                // define private access level for the employee
+                System.out.println("Private access level defined for " + e.getUserName());
+                break;
+            case "default":
+                // define default access level for the employee
+                System.out.println("Default access level defined for " + e.getUserName());
+                break;
+            case "protected":
+                // define protected access level for the employee
+                System.out.println("Protected access level defined for " + e.getUserName());
+                break;
+            case "public":
+                // define public access level for the employee
+                System.out.println("Public access level defined for " + e.getUserName());
+                break;
+            default:
+                System.out.println("Invalid access level. Please enter a valid access level.");
+                break;
+        }
     }
+
     void viewEmployees() {
         for(Employee e: a) {
                 // Print employee data
@@ -31,15 +52,30 @@ public class Administrator extends User {
                 System.out.println("Employee Department: " + e.getQualification());
         }
     }
-    void editEmployee(int nuserID,String newUserName,String npassword,String nuserRole,String ndepartment, String ncontact, String nnationality, String nqualification) {
-        Employee employee = getEmployeeByID(userID);
-        if (employee != null) {
-            employee.updateDetailsEmployee(nuserID,newUserName,npassword,nuserRole,ndepartment,ncontact,nnationality,nqualification);
-            System.out.println("Employee details updated successfully.");
-        } else {
-            System.out.println("Employee not found.");
+    void viewEmployee(int id) {
+        for(Employee e: a) {
+            if(e.userID==id){
+                System.out.println("Employee ID: " + e.getUserID());
+                System.out.println("Employee Name: " + e.getUserName());
+                System.out.println("Employee userRole: " + e.getUserRole());
+                System.out.println("Employee Department: " + e.getDepartment());
+                System.out.println("Employee Contact: " + e.getContact());
+                System.out.println("Employee Nationality: " + e.getNationality());
+                System.out.println("Employee Qualification: " + e.getQualification());
+            }
         }
     }
+    void editEmployee(int nuserID, String newUserName, String npassword, String nuserRole, String ndepartment, String ncontact, String nnationality, String nqualification) {
+        for (Employee e : a) {
+            if (e.userID == nuserID) {
+                e.updateDetailsEmployee(nuserID, newUserName, npassword, nuserRole, ndepartment, ncontact, nnationality, nqualification);
+                System.out.println("Employee details updated successfully.");
+                return;
+            }
+        }
+        System.out.println("Employee not found.");
+    }
+
     void newLogin(String uname,String pass){
         if(Objects.equals(this.userName, uname) && Objects.equals(this.password, pass)){
             System.out.println("You are logged in");
